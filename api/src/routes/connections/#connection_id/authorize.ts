44edb78618bc46d899c0b3657b9e76e8c8699958
Puyodead1 <puyodead@protonmail.com>
2022-08-08 23:53:30 -0400

    update errors to be more relevant

--- api/src/routes/connections/#connection_id/authorize.ts
+++ api/src/routes/connections/#connection_id/authorize.ts
@@ -1,15 +1,28 @@
 import { Router, Request, Response } from "express";
 import { Connections, route } from "@fosscord/api";
-import { HTTPError } from "lambert-server";
 import { BaseConnection } from "../../../connections/BaseConnection";
+import { FieldErrors } from "@fosscord/util";
 
 const router = Router();
 
 router.get("/", route({}), async (req: Request, res: Response) => {
 	const { connection_id } = req.params;
 	const connection: BaseConnection = Connections.connections[connection_id];
-	if (!connection) throw new HTTPError("Unknown connection", 400);
-	if (!connection.enabled) throw new HTTPError("Connection is not available", 400);
+	if (!connection)
+		throw FieldErrors({
+			provider_id: {
+				code: "BASE_TYPE_CHOICES",
+				message: req.t("common:field.BASE_TYPE_CHOICES", {
+					types: Object.keys(Connections.connections).join(", ")
+				})
+			}
+		});
+	if (!connection.enabled)
+		throw FieldErrors({
+			provider_id: {
+				message: "This connection has been disabled server-side."
+			}
+		});
 
 	res.json({
 		url: connection.makeAuthorizeUrl()
--- api/src/routes/connections/#connection_id/callback.ts
+++ api/src/routes/connections/#connection_id/callback.ts
@@ -1,7 +1,6 @@
 import { Router, Request, Response } from "express";
 import { ConnectionAuthCallbackSchema, Connections, route } from "@fosscord/api";
-import { HTTPError } from "lambert-server";
-import { emitEvent, UserConnectionsUpdateEvent } from "@fosscord/util";
+import { emitEvent, FieldErrors, UserConnectionsUpdateEvent } from "@fosscord/util";
 import { BaseConnection } from "../../../connections/BaseConnection";
 
 const router = Router();
@@ -11,8 +10,21 @@ router.post("/", route({ body: "ConnectionAuthCallbackSchema" }), async (req: Re
 
 	const { connection_id } = req.params;
 	const connection: BaseConnection = Connections.connections[connection_id];
-	if (!connection) throw new HTTPError("Unknown connection", 400);
-	if (!connection.enabled) throw new HTTPError("Connection is not available", 400);
+	if (!connection)
+		throw FieldErrors({
+			provider_id: {
+				code: "BASE_TYPE_CHOICES",
+				message: req.t("common:field.BASE_TYPE_CHOICES", {
+					types: Object.keys(Connections.connections).join(", ")
+				})
+			}
+		});
+	if (!connection.enabled)
+		throw FieldErrors({
+			provider_id: {
+				message: "This connection has been disabled server-side."
+			}
+		});
 
 	const token = await connection.exchangeCode(body.code, body.state);
 	const userInfo = await connection.getUser(token);
