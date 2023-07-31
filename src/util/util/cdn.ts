542a419cb5bea42052e5a2c8da11a3dbe710369d
ochen1 <o.chen1@share.epsb.ca>
2023-07-31 17:55:29 -0600

    test

--- src/util/util/cdn.ts
+++ src/util/util/cdn.ts
@@ -46,7 +46,12 @@ export async function uploadFile(
 			body: form,
 		},
 	);
-	const result = (await response.json()) as Attachment;
+
+	const tmp = await response.json();
+	tmp.url = tmp.url.startsWith("http://localhost:3001")
+        ? tmp.url.slice("http://localhost:3001".length)
+		: tmp.url;
+	const result = (tmp) as Attachment;
 
 	if (response.status !== 200) throw result;
 	return result;
